83cb19419c2fd1bb10a941ac6e5c8b55a83b23a0
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-06-23 20:50:40 +0200

    june 23

--- leetcode/subsequences/src/main.rs
+++ leetcode/subsequences/src/main.rs
@@ -10,7 +10,8 @@
 
 fn main() {
     let input = String::from("abcde");
-    let words:Vec<String> = vec!["a".to_string(),"bb".to_string(),"acd".to_string(),"ace".to_string()];
+    //let words:Vec<String> = vec!["a".to_string(),"bb".to_string(),"acd".to_string(),"ace".to_string()];
+    let words:Vec<String> = vec!["a".to_string(),"bb".to_string()];
 
 
   num_matching_subseq(input, words);
@@ -18,8 +19,28 @@ fn main() {
 
 pub fn num_matching_subseq(s: String, words: Vec<String>) -> i32 {
     let s_to_vec: Vec<char> = s.chars().collect::<Vec<_>>();
+    let mut counter = 0;
 
-    println!("test {:?}", s_to_vec);
+    println!("input es {:?}", s_to_vec);
+
+    for i in words {
+      let i_to_vec: Vec<char> = i.chars().collect::<Vec<_>>();
+      println!("el vector: {:?}", i_to_vec);
+      for (i, &x) in i_to_vec.iter().enumerate() {
+        println! ("el indice es {} y el valor {}", &i, &x);
+        for (y, &z) in s_to_vec.iter().enumerate() {
+          counter += 1;
+          println! ("iteracion {} -> letra {} con {}", counter, x, z);
+           if x == z {
+             println! ("{} igual a {}", x, z );
+
+           }
+           else {
+             println!("no es igual");
+           }
+        }
+      }
+    }
 
     32
 }
